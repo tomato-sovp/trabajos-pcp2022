d27abfa4f17d354ee6d150765cc1361f1f3962b3
tomato-sovp <103199307+tomato-sovp@users.noreply.github.com>
2022-04-14 10:39:32 -0400

    Add files via upload

---
+++ LAB05.py
@@ -0,0 +1,36 @@
+##Lab 05
+def mult_esc(lista,constante):
+    lista[0][0]=lista[0][0]*constante
+    lista[0][1]=lista[0][1]*constante
+    lista[0][2]=lista[0][2]*constante
+    lista[1][0]=lista[1][0]*constante
+    lista[1][1]=lista[1][1]*constante
+    lista[1][2]=lista[1][2]*constante
+    lista[2][0]=lista[2][0]*constante
+    lista[2][1]=lista[2][1]*constante
+    lista[2][2]=lista[2][2]*constante
+    return lista
+
+def suma_list(lista_1,lista_2):
+    lista_1[0][0]=lista_1[0][0]+lista_2[0][0]
+    lista_1[0][1]=lista_1[0][1]+lista_2[0][1]
+    lista_1[0][2]=lista_1[0][2]+lista_2[0][2]
+    lista_1[1][0]=lista_1[1][0]+lista_2[1][0]
+    lista_1[1][1]=lista_1[1][1]+lista_2[1][1]
+    lista_1[1][2]=lista_1[1][2]+lista_2[1][2]
+    lista_1[2][0]=lista_1[2][0]+lista_2[2][0]
+    lista_1[2][1]=lista_1[2][1]+lista_2[2][1]
+    lista_1[2][2]=lista_1[2][2]+lista_2[2][2]
+    return lista_1
+def mult_list(lista_1,lista_2):
+    lista
+
+A=[[11,12,13],
+   [21,22,23],
+   [31,32,33]]
+B=[[11,12,13],
+   [21,22,23],
+   [31,32,33]]
+escalar=3
+
+print()
